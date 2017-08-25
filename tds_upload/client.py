c680b9025ee0ae07587a49d12b077a26c0353e23
Mac Coombe <Mac.Coombe@csiro.au>
2017-08-25 13:01:39 +1000

    Initial commit.

---
+++ tds_upload/client.py
@@ -0,0 +1,56 @@
+
+import os, posixpath, requests, warnings
+
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
+
+# If possible, use the MultipartEncoded from the Requests toolbelt to allow for
+# streaming upload.
+try:
+    from requests_toolbelt import MultipartEncoder
+    
+    def _prepare_multipart_request(fields):
+        m = MultipartEncoder(fields=fields)
+        
+        return {
+            'data': m,
+            'headers': {
+                'Content-Type': m.content_type
+            }
+        }
+# Otherwise, fall back to Request's default behaviour.
+except ImportError:
+    warnings.warn('Using default multipart request encoder - upload size will be limited by available RAM.')
+    
+    def _prepare_multipart_request(fields):
+        return { 'files': fields }
+
+class Client(object):
+    def __init__(self, url, session=None):
+        self._url = url
+        self._session = session or requests.Session()
+    
+    def upload_data(self, data, path, id=None, name=None, organisation_id=None, group_ids=None):
+        with open(data, 'rb') as f:
+            fields = {
+                'id': id,
+                'name': name,
+                'organisationid': organisation_id,
+                'groupids': group_ids,
+                'path': path,
+                'data': f
+            }
+            
+            fields = { k:v for k,v in fields.iteritems() if v is not None }
+            
+            request = _prepare_multipart_request(fields)
+            
+            response = self._session.post(self._get_endpoint('data'), **request)
+            response.raise_for_status()
+    
+    def _get_endpoint(self, endpoint):
+        parts = list(urlparse.urlparse(self._url))
+        parts[2] = posixpath.join(parts[2], endpoint)
+        return urlparse.urlunparse(parts)
